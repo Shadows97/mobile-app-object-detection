da2186d158526259bca2bc6348f0bf30533f2cf6
Derrick M'PO <mpoderrick97@gmail.com>
2021-03-23 19:00:42 +0100

    first commit

--- src/constant.js
+++ src/constant.js
@@ -1,18 +1,18 @@
 export const OPTIONS = {
-  keyPrefix: "uploads/",
-  bucket: "objectdetecttest23",
-  region: "us-east-1",
-  accessKey: "AKIAX7X4PVNC2DYUVY4X",
-  secretKey: "n/cOIfGw20wZOQAzIdvpWE7fu/5f2r1baa4iX1CS",
-  successActionStatus: 201
-}
+    keyPrefix: "uploads/",
+    bucket: "your bucket name",
+    region: "us-east-1",
+    accessKey: "your access key",
+    secretKey: "your secret key",
+    successActionStatus: 201
+  }
 
 
 export const API_CONFIG = {
-  name: 'detection',   //your api name
-  endpoint:"https://kz45yruhvj.execute-api.us-east-1.amazonaws.com/test", //Your Endpoint URL
- 
-}
+    name: 'detection',   //your api name
+    endpoint:"", //Your Endpoint URL
+   
+  }
 
 
 export const API_NAME = 'detection';
